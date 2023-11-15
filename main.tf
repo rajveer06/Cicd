c0d63fede4096fec237ef05d336a7310182f20db
rajveer06 <rajveersinghchouha11@gmail.com>
2023-11-15 11:55:29 +0530

    third commit

--- main.tf
+++ main.tf
@@ -5,3 +5,12 @@ provider "aws" {
 resource "aws_vpc" "TestCicdVpc" {
   cidr_block = "10.0.0.0/16"
 }
+
+resource "aws_instance" "example" {
+  ami           = "ami-0287a05f0ef0e9d9a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Rajveer"
+  }
+}
